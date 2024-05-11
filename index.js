e4d08b800fa7293454fa0c4a71250472d9f2d5f3
Rohit Roy Chowdhury <roychowdhuryrohit.dev@gmail.com>
2024-05-11 12:43:24 -0700

    Add ollama support

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 #!/usr/bin/env node
 // This shebang line is important. It tells the OS to use Node to run this script.
 
-console.log('Hello from expose AI!');
+import ollama from "ollama";
+
+import readline from 'node:readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question(`How can I help you?\n`, async prompt => {
+    const message = { role: 'user', content: prompt }
+    const response = await ollama.chat({ model: 'llama3', messages: [message], stream: true })
+    for await (const part of response) {
+        process.stdout.write(part.message.content)
+    }
+    rl.close();
+});
+
+
 
